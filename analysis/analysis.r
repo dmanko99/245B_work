b1d81cdaa4079c7a8acabbf331a729dd5645d9eb
Brandon <bw@Brandons-MacBook-Pro.local>
2020-05-27 14:33:59 -0500

    edits to analysis script

--- analysis/analysis.r
+++ analysis/analysis.r
@@ -8,7 +8,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # LOAD DATA
 
-d <- read.csv("ssi-trials.csv") %>%
+d <- read.csv("../data/initial_pilot2.csv") %>%
   filter(slide_type != "bot_check")
 
 d$response <- as.numeric(d$response)
@@ -38,11 +38,45 @@ blacklist <- unique(wrong_responses$workerid)
 d <- d %>%
   filter(!(workerid %in% blacklist))
 
+# CODE NAME CATEGORY
+
+d$nameCategory <- factor(d$first %in% c("Trevon",
+                                 "Tyree",
+                                 "Deion",
+                                 "Marquis",
+                                 "Jermaine",
+                                 "Lamont",
+                                 "Tyrone",
+                                 "Deandre",
+                                 "Tremayne",
+                                 "Lamar",
+                                 "Kareem",
+                                 "Hakeem",
+                                 "Jamal",
+                                 "Rasheed",
+                                 "Deshawn"))
+
+# SANITY CHECK: TRUE AND FALSE SHOULD BE EQUAL
+
+d %>%
+  filter(type != "exclusion") %>%
+  group_by(nameCategory) %>%
+  summarize(n = n())
+
+levels(d$nameCategory) <- c("white", "black")
+
+# RENAME COLUMNS
+
+d <- d %>%
+  rename(stereotype = tag,
+         item = story,
+         name = first)
+
 # VISUALIZE RESPONSE BY STEREOTYPE AND NAME
 
-byStereotypeName <- d %>%
+byStereotypeNameCategory <- d %>%
   filter(type == "critical") %>%
-  group_by(first, tag) %>%
+  group_by(nameCategory, stereotype) %>%
   summarize(Mean = mean(response), 
             CILow =ci.low(response),
             CIHigh =ci.high(response)) %>%
@@ -50,20 +84,20 @@ byStereotypeName <- d %>%
   mutate(YMin = Mean - CILow,
          YMax = Mean + CIHigh)
 
-ggplot(byStereotypeName, aes(x=first, y=Mean, fill = tag)) +
+ggplot(byStereotypeNameCategory, aes(x=nameCategory, y=Mean, fill = stereotype)) +
   # facet_wrap(~kind, scales = "free") +
   theme_bw() +
   scale_fill_grey() +
   geom_bar(stat="identity",position = "dodge") +
   theme(axis.text.x=element_text(angle=20,hjust=1,vjust=1)) +
   geom_errorbar(aes(ymin=YMin,ymax=YMax),size = 0.25,width= 0.025,position = dodge) +  
-  labs(x = "Name", y = "Mean rating", fill = "Stereotype") 
+  labs(x = "Race", y = "Mean rating", fill = "Stereotype") 
 
 # VISUALIZE RESPONSE BY NAME AND ITEM
 
-byNameItem <- d %>%
+byNameCategoryItem <- d %>%
   filter(type == "critical") %>%
-  group_by(first, story) %>%
+  group_by(nameCategory, item, stereotype) %>%
   summarize(Mean = mean(response), 
             CILow =ci.low(response),
             CIHigh =ci.high(response)) %>%
@@ -71,27 +105,28 @@ byNameItem <- d %>%
   mutate(YMin = Mean - CILow,
          YMax = Mean + CIHigh)
 
-ggplot(byNameItem, aes(x=story, y=Mean, fill = first)) +
-  # facet_wrap(~kind, scales = "free") +
-  theme_bw() +
-  scale_fill_grey() +
-  geom_bar(stat="identity",position = "dodge") +
-  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=1)) +
-  geom_errorbar(aes(ymin=YMin,ymax=YMax),size = 0.25,width= 0.025,position = dodge) +  
-  labs(x = "Item", y = "Mean rating", fill = "Name") 
+byNameCategoryItem %>%
+  # CAN BE 'evoke' or 'repress'
+  filter(stereotype == "repress") %>%
+  ggplot(aes(x=item, y=Mean, fill = nameCategory)) +
+    theme_bw() +
+    scale_fill_grey() +
+    geom_bar(stat="identity",position = "dodge") +
+    theme(axis.text.x=element_text(angle=90,hjust=1,vjust=1)) +
+    geom_errorbar(aes(ymin=YMin,ymax=YMax),size = 0.25,width= 0.025,position = dodge) +  
+    labs(x = "Item", y = "Mean rating", fill = "Name") 
 
 # MEAN CENTERING AND BRMS ANALYSIS
 
 d$response_ctr <- scale(d$response, center = TRUE, scale = FALSE)
 d$response_ctr <- d$response_ctr - mean(d$response_ctr)
 
-d$first = relevel(d$first, ref = "Conner")
-d$tag = relevel(d$tag, ref = "repress")
+d$stereotype = relevel(d$stereotype, ref = "repress")
 
 m <- brm(
-  response_ctr ~ first + tag + first * tag + (1 + first|story) + (1 + (first + tag + first * tag)|workerid), 
+  response_ctr ~ nameCategory * stereotype + (1 + nameCategory|item) + (1 + stereotype|name) + (1 + nameCategory*stereotype|workerid), 
   data = d %>% filter(type == "critical"),
-  control = list(adapt_delta = 0.99, max_treedepth = 15),
+  # control = list(adapt_delta = 0.99, max_treedepth = 15),
   family = gaussian(),
   seed = 123
 )

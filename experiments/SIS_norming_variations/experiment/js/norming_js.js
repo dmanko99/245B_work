e26fdf3146a2aea6dd3c1a9ea4ef3909b85a758d
Dean Manko <dmanko@stanford.edu>
2020-06-22 15:35:54 -0400

    adding norming variations

---
+++ experiments/SIS_norming_variations/experiment/js/norming_js.js
@@ -0,0 +1,315 @@
+function make_slides(f) {
+  var   slides = {};
+
+  slides.bot = slide({
+    name : "bot",
+    start: function() {
+      $('.err1').hide();
+      $('.err2').hide();
+      $('.disq').hide();
+      exp.speaker = _.shuffle(["James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Thomas", "Charles"])[0];
+      exp.listener = _.shuffle(["Mary", "Patricia", "Jennifer", "Linda", "Elizabeth", "Barbara", "Susan", "Jessica", "Sarah", "Margaret"])[0];
+      exp.lives = 0;
+      var story = exp.speaker + ' says to ' + exp.listener + ': "It\'s a beautiful day, isn\'t it?"'
+      var question = 'Who does ' + exp.speaker + ' talk to?';
+      document.getElementById("s").innerHTML = story;
+      document.getElementById("q").innerHTML = question;
+    },
+    button : function() {
+      exp.text_input = document.getElementById("text_box").value;
+      var lower = exp.listener.toLowerCase();
+
+      if ((exp.lives < 3) && ((exp.text_input.toLowerCase() == lower))){
+        exp.data_trials.push({
+          "slide_number": exp.phase,
+          "slide_type" : "bot_check",
+          "image" : exp.listener,
+          "audio" : "",
+          "response" : [0,exp.text_input]
+        });
+        exp.go();
+      }
+      else {
+        exp.data_trials.push({
+          "slide_number": exp.phase,
+          "slide_type" : "bot_check",
+          "image" : exp.listener,
+          "audio" : "",
+          "response" : [0,exp.text_input]
+        });
+        if (exp.lives == 0){
+          $('.err1').show();
+        }if (exp.lives == 1){
+          $('.err1').hide();
+          $('.err2').show();
+        }if (exp.lives == 2){
+          $('.err2').hide();
+          $('.disq').show();
+          $('.button').hide();
+        }
+        exp.lives++;
+      }   
+    },
+  });
+
+  slides.i0 = slide({
+     name : "i0",
+     start: function() {
+      exp.startT = Date.now();
+     }
+  });
+
+  // slides.reqs = slide({
+  //   name : "reqs",
+  //   start: function() {
+  //     $(".err").hide();
+  //   },
+  //   button : function() {
+  //     if (document.getElementById("american_check").checked == false) {
+  //     //if ($('input[name="american_check"]:checked').val() == undefined) {
+  //       $(".err").show();
+  //     } else {
+  //     exp.go(); //make sure this is at the *end*, after you log your data
+  //     }
+  //   }
+  // });
+
+  // slides.irb_info = slide({
+  //    name : "irb_info",
+  //    button: function() {
+  //     exp.go();
+  //    }
+  // });
+
+  slides.instructions = slide({
+    name : "instructions",
+    button : function() {
+      exp.go(); //use exp.go() if and only if there is no "present" data.
+    }
+  });
+
+  slides.main_task = slide({
+    name: "main_task",
+
+    present : exp.stims,
+
+    present_handle : function(stim) {
+      var prompt, utt;
+      //a bunch of stuff goes here!!!
+
+      // "uncheck" radio buttons
+      $('input[name="speaker"]').prop('checked', false);
+
+      // hide error message
+      $(".err").hide();
+
+      // show "data"
+      $(".data").show();
+
+      // get index number of trial
+      this.trialNum = exp.stimscopy.indexOf(stim);
+
+      // record trial start time
+      this.startTime = Date.now();
+
+      // storing this info in the slide so I can record it later?
+      this.stim = stim; 
+
+      // replace NAME from stimuli
+      var reminder = stim.first;
+      var story = replaceTerms(this.stim, "storyline")
+
+      //display story-dependent fields
+      if(stim.stimType != "exclusion") {
+        //if male
+        document.getElementById('reminder').innerHTML = reminder + " is an American man.";
+        //if female
+        // document.getElementById('reminder').innerHTML = reminder + " is an American woman.";
+      } else {
+        document.getElementById('reminder').innerHTML = "";
+      }
+      document.getElementById('output').innerHTML = story;
+
+      this.init_sliders();
+      
+      //erase current slider value
+      exp.sliderPost = null;
+    },
+
+    button : function() {
+      if (exp.sliderPost == null) {
+        $(".err").show();
+      } else {
+        this.log_responses();
+
+        /* use _stream.apply(this); if and only if there is
+        "present" data. (and only *after* responses are logged) */
+        _stream.apply(this);
+      }
+    },
+
+    init_sliders : function() {
+      utils.make_slider("#single_slider", function(event, ui) {
+        exp.sliderPost = ui.value;
+      });
+    },
+
+    log_responses : function() {
+      exp.data_trials.push({
+        "trial_num": this.trialNum,
+        "response" : exp.sliderPost,
+        "first": this.stim.first,
+        "story": this.stim.story,
+        "scale": scales[this.stim.scaleType],
+        "tag": this.stim.tag,
+        "list" : exp.currentList,
+        "type" : this.stim.stimType
+      });
+    }
+  });
+
+  slides.subj_info =  slide({
+    name : "subj_info",
+    submit : function(e){
+      //if (e.preventDefault) e.preventDefault(); // I don't know what this means.
+      var raceData = new Array();
+      var raceQs = document.getElementById("checkboxes");
+      var chks = raceQs.getElementsByTagName("INPUT");
+      for (var i = 0; i < chks.length; i++) {
+        if (chks[i].checked) {
+          raceData.push(chks[i].value);
+        }
+      };
+
+      exp.subj_data = {
+        language : $("#language").val(),
+        enjoyment : $("#enjoyment").val(),
+        asses : $('input[name="assess"]:checked').val(),
+        age : $("#age").val(),
+        gender : $("#gender").val(),
+        education : $("#education").val(),
+        affiliation : $("#affiliation").val(),
+        race : raceData.join(", "),
+        comments : $("#comments").val(),
+        problems: $("#problems").val(),
+        fairprice: $("#fairprice").val()
+      };
+      exp.go(); //use exp.go() if and only if there is no "present" data.
+    }
+  });
+
+  slides.thanks = slide({
+    name : "thanks",
+    start : function() {
+      exp.data= {
+          "trials" : exp.data_trials,
+          "catch_trials" : exp.catch_trials,
+          "system" : exp.system,
+          "condition" : exp.condition,
+          "subject_information" : exp.subj_data,
+          "time_in_minutes" : (Date.now() - exp.startT)/60000
+      };
+      setTimeout(function() {turk.submit(exp.data);}, 1000);
+    }
+  });
+
+  return slides;
+}
+
+/// init ///
+function init() {
+  exp.trials = [];
+  exp.catch_trials = [];
+
+  // actually there are 24 trials, but 20 require FIRST-replacement
+  exp.nTrials = 20;
+
+  exp.stims = [];
+  //var labels = _.shuffle(predicates);
+  //var 
+
+  // determine what list to serve to participant
+  exp.currentList = _.shuffle([1,2,3])[0]
+
+  // filter criticals by list
+  var listCriticals = criticals.filter(function (stim) {
+  return stim.list == exp.currentList
+  });
+
+  // stories are the critical items for the list, plus fillers
+  // vacuous call to filter just converts json to javascript object
+  var stories = listCriticals.concat(fillers.filter(function() { return true } ))
+
+  exp.stories = stories
+
+  for (var i=0; i<exp.nTrials; i++) { //male names
+    var f;
+    f = {
+      first: firsts[i],
+      //scale: _.shuffle(scales)[i]
+    }
+    exp.stims.push(
+      _.extend(stories[i], f)
+    )
+  };
+
+  // for (var i=0; i<exp.nTrials; i++) { //female names
+  //     var f;
+  //     f = {
+  //       first: firstsF[i],
+  //       //scale: _.shuffle(scales)[i]
+  //     }
+  //     exp.stims.push(
+  //       _.extend(stories[i], f)
+  //     )
+  //   };
+      
+
+  // add exclusion stims
+  exp.stims = exp.stims.concat(exclusions.filter(function() { return true } ))
+
+  exp.stims = _.shuffle(exp.stims);
+
+  exp.stimscopy = exp.stims.slice(0);
+
+  exp.system = {
+      Browser : BrowserDetect.browser,
+      OS : BrowserDetect.OS,
+      screenH: screen.height,
+      screenUH: exp.height,
+      screenW: screen.width,
+      screenUW: exp.width
+    };
+
+  //blocks of the experiment:
+  exp.structure=[
+    "bot",
+    "i0", 
+    // "reqs",
+    "instructions",
+    "main_task",
+    "subj_info",
+    "thanks"
+  ];
+
+  exp.data_trials = [];
+  //make corresponding slides:
+  exp.slides = make_slides(exp);
+
+  exp.nQs = utils.get_exp_length(); //this does not work if there are stacks of stims (but does work for an experiment with this structure)
+                    //relies on structure and slides being defined
+
+  $('.slide').hide(); //hide everything
+
+  //make sure turkers have accepted HIT (or you're not in mturk)
+  $("#start_button").click(function() {
+    if (turk.previewMode) {
+      $("#mustaccept").show();
+    } else {
+      $("#start_button").click(function() {$("#mustaccept").show();});
+      exp.go();
+    }
+  });
+
+  exp.go(); //show first slide
+}
---
+++ experiments/SIS_norming_variations/experiment/js/norming_stims.js
@@ -0,0 +1,620 @@
+
+var wnames = ["Peter",
+"Brad",
+"Ethan",
+"Ian",
+"Cody",
+"Brett",
+"Paul",
+"Connor",
+"Jack",
+"Logan",
+"Roger",
+"Dylan",
+"Hunter",
+"Dustin",
+"Ryan"]
+
+var bnames = ["Trevon",
+"Tyree",
+"Deion",
+"Marquis",
+"Jermaine",
+"Lamont",
+"Tyrone",
+"Deandre",
+"Tremayne",
+"Lamar",
+"Kareem",
+"Hakeem",
+"Jamal",
+"Rasheed",
+"Deshawn"]
+
+var wFnames = ["Emily",
+"Hannah",
+"Lauren",
+"Megan",
+"Rachel",
+"Rebecca",
+"Samantha",
+"Sarah",
+"Victoria",
+"Anne",
+"Jill",
+"Allison",
+"Laurie",
+"Meredith",
+"Carrie"]
+
+var bFnames = ["Aisha",
+"Keisha",
+"Tamika",
+"Lakisha",
+"Tanisha",
+"Latoya",
+"Kenya",
+"Latonya",
+"Ebony",
+"Tiara",
+"Monique",
+"Jasmine",
+"Desiree",
+"Shante",
+"Tanisha"]
+
+var firsts = _.shuffle(_.sample(wnames, 10).concat(_.sample(bnames, 10)))
+var firstsF = _.shuffle(_.sample(wFnames, 10).concat(_.sample(bFnames, 10)))
+
+var criticals = [
+	{
+		"story":"or/and stealing",
+		"storyline":"FIRST has stolen a car and a bicycle.",
+		"tag":"evoke",
+		"scaleType":0,
+		"stimType":"critical",
+		"list":1
+	},{
+		"story":"or/and violent crime",
+		"storyline":"FIRST has been found guilty of theft and violent crime.",
+		"tag":"evoke",
+		"scaleType":0,
+		"stimType":"critical",
+		"list":2
+	},{
+		"story":"or/and threat",
+		"storyline":"FIRST has threatened a judge and a police officer.",
+		"tag":"evoke",
+		"scaleType":0,
+		"stimType":"critical",
+		"list":3
+	},{
+		"story":"or/and not stealing",
+		"storyline":"FIRST has had his car and his bicycle stolen.",
+		// "storyline":"FIRST has had her car and her bicycle stolen.",
+		"tag":"repress",
+		"scaleType":0,
+		"stimType":"critical",
+		"list":3
+	},{
+		"story":"or/and not violent crime",
+		"storyline":"FIRST has been a victim of theft and violence.",
+		"tag":"repress",
+		"scaleType":0,
+		"stimType":"critical",
+		"list":1
+	},{
+		"story":"or/and not police",
+		"storyline":"FIRST has worked as a judge and a police officer.",
+		"tag":"repress",
+		"scaleType":0,
+		"stimType":"critical",
+		"list":2
+	},{
+		"story":"some/all criminal",
+		"storyline":"All of FIRST's friends are criminals.",
+		"tag":"evoke",
+		"scaleType":1,
+		"stimType":"critical",
+		"list":1
+	},{
+		"story":"some/all violent",
+		//"storyline":"All of FIRST's brothers are violent.",
+		"storyline":"All of FIRST's sisters are violent.",
+		"tag":"evoke",
+		"scaleType":1,
+		"stimType":"critical",
+		"list":2
+	},{
+		"story":"some/all prison",
+		"storyline":"All of FIRST's cousins have spent time in prison.",
+		"tag":"evoke",
+		"scaleType":1,
+		"stimType":"critical",
+		"list":3
+	},{
+		"story":"some/all respect",
+		"storyline":"All of FIRST's friends respect the police.",
+		"tag":"repress",
+		"scaleType":1,
+		"stimType":"critical",
+		"list":3
+	},{
+		"story":"some/all police",
+		//"storyline":"All of FIRST's brothers are police officers.",
+		"storyline":"All of FIRST's sisters are police officers.",
+		"tag":"repress",
+		"scaleType":1,
+		"stimType":"critical",
+		"list":1
+	},{
+		"story":"some/all prison work",
+		"storyline":"All of FIRST's cousins work at a prison.",
+		"tag":"repress",
+		"scaleType":1,
+		"stimType":"critical",
+		"list":2
+	},{
+		"story":"looks like/is criminal",
+		"storyline":"FIRST is a criminal.",
+		"tag":"evoke",
+		"scaleType":2,
+		"stimType":"critical",
+		"list":1
+	},{
+		"story":"looks like/is thief",
+		"storyline":"FIRST is a thief.",
+		"tag":"evoke",
+		"scaleType":2,
+		"stimType":"critical",
+		"list":2
+	},{
+		"story":"looks like/is dangerous",
+		"storyline":"FIRST is a dangerous person.",
+		"tag":"evoke",
+		"scaleType":2,
+		"stimType":"critical",
+		"list":3
+	},{
+		"story":"looks like/is police",
+		"storyline":"FIRST is a police officer.",
+		"tag":"repress",
+		"scaleType":2,
+		"stimType":"critical",
+		"list":3
+	},{
+		"story":"looks like/is nonviolent",
+		"storyline":"FIRST is a nonviolent person.",
+		"tag":"repress",
+		"scaleType":2,
+		"stimType":"critical",
+		"list":1
+	},{
+		"story":"looks like/is respect",
+		"storyline":"FIRST is someone who respects the law.",
+		"tag":"repress",
+		"scaleType":2,
+		"stimType":"critical",
+		"list":2
+	},{
+		"story":"possible/certain jail",
+		"storyline":"FIRST has spent time in prison.",
+		"tag":"evoke",
+		"scaleType":3,
+		"stimType":"critical",
+		"list":1
+	},{
+		"story":"possible/certain stealing",
+		"storyline":"FIRST has stolen a TV. ",
+		"tag":"evoke",
+		"scaleType":3,
+		"stimType":"critical",
+		"list":2
+	},{
+		"story":"possible/certain mugged",
+		"storyline":"FIRST has mugged someone in a local park.",
+		"tag":"evoke",
+		"scaleType":3,
+		"stimType":"critical",
+		"list":3
+	},{
+		"story":"possible/certain police",
+		"storyline":"FIRST would make a good police officer.",
+		"tag":"repress",
+		"scaleType":3,
+		"stimType":"critical",
+		"list":3
+	},{
+		"story":"possible/certain not violence",
+		"storyline":"FIRST avoids violent people.",
+		"tag":"repress",
+		"scaleType":3,
+		"stimType":"critical",
+		"list":1
+	},{
+		"story":"possible/certain not mugged",
+		"storyline":"FIRST has been mugged in a local park. ",
+		"tag":"repress",
+		"scaleType":3,
+		"stimType":"critical",
+		"list":2
+	},{
+		"story":"n/n+m crime",
+		"storyline":"FIRST has committed more than three violent crimes.",
+		"tag":"evoke",
+		"scaleType":4,
+		"stimType":"critical",
+		"list":1
+	},{
+		"story":"n/n+m prison",
+		"storyline":"FIRST has been sent to prison more than three times.",
+		"tag":"evoke",
+		"scaleType":4,
+		"stimType":"critical",
+		"list":2
+	},{
+		"story":"n/n+m murder",
+		"storyline":"FIRST has threatened to kill more than three people.",
+		"tag":"evoke",
+		"scaleType":4,
+		"stimType":"critical",
+		"list":3
+	},{
+		"story":"n/n+m stolen",
+		"storyline":"FIRST has had his wallet stolen more than three times.",
+		//"storyline":"FIRST has had her purse stolen more than three times.",
+		"tag":"repress",
+		"scaleType":4,
+		"stimType":"critical",
+		"list":3
+	},{
+		"story":"n/n+m victims",
+		"storyline":"FIRST knows more than three victims of violent crime.",
+		"tag":"repress",
+		"scaleType":4,
+		"stimType":"critical",
+		"list":1
+	},{
+		"story":"n/n+m police",
+		"storyline":"FIRST has more than three relatives who are police officers.",
+		"tag":"repress",
+		"scaleType":4,
+		"stimType":"critical",
+		"list":2
+	}]
+
+var exclusions = [
+	{
+		"story":"math1",
+		"storyline":"5 + 4 = 9",
+		"tag":"exclusion_right",
+		"scaleType":9,
+		"stimType":"exclusion",
+		"list":"all"
+	},{
+		"story":"math2",
+		"storyline":"10 - 5 = 5",
+		"tag":"exclusion_right",
+		"scaleType":9,
+		"stimType":"exclusion",
+		"list":"all"
+	},{
+		"story":"math3",
+		"storyline":"10 + 2 = 13",
+		"tag":"exclusion_wrong",
+		"scaleType":9,
+		"stimType":"exclusion",
+		"list":"all"
+	},{
+		"story":"math4",
+		"storyline":"5 - 3 = 1",
+		"tag":"exclusion_wrong",
+		"scaleType":9,
+		"stimType":"exclusion",
+		"list":"all"}]
+
+var fillers = [
+	{
+		"story":"sports",
+		"storyline":"FIRST watches sports.",
+		"tag":"filler_highbias",
+		"scaleType":9,
+		"stimType":"filler",
+		"list":"all"
+	},{
+		"story":"dog",
+		"storyline":"FIRST owns a dog or has owned a dog. ",
+		"tag":"filler_highbias",
+		"scaleType":9,
+		"stimType":"filler",
+		"list":"all"
+	},{
+		"story":"sister",
+		"storyline":"FIRST has at least one sister.",
+		"tag":"filler_highbias",
+		"scaleType":9,
+		"stimType":"filler",
+		"list":"all"
+	},{
+		"story":"alcohol",
+		"storyline":"FIRST drinks alcohol with his friends. ",
+		//"storyline":"FIRST drinks alcohol with her friends. ",
+		"tag":"filler_highbias",
+		"scaleType":9,
+		"stimType":"filler",
+		"list":"all"
+	},{
+		"story":"movies",
+		"storyline":"FIRST enjoys watching movies. ",
+		"tag":"filler_highbias",
+		"scaleType":9,
+		"stimType":"filler",
+		"list":"all"
+	},{
+		"story":"plane crash",
+		"storyline":"FIRST has been in a plane crash and survived.",
+		"tag":"filler_lowbias",
+		"scaleType":9,
+		"stimType":"filler",
+		"list":"all"
+	},{
+		"story":"lottery",
+		"storyline":"FIRST has won the lottery twice.",
+		"tag":"filler_lowbias",
+		"scaleType":9,
+		"stimType":"filler",
+		"list":"all"
+	},{
+		"story":"dentist",
+		"storyline":"FIRST looks forward to his dental appointments.",
+		//"storyline":"FIRST looks forward to her dental appointments.",
+		"tag":"filler_lowbias",
+		"scaleType":9,
+		"stimType":"filler",
+		"list":"all"
+	},{
+		"story":"lightning",
+		"storyline":"FIRST has been struck by lightning.",
+		"tag":"filler_lowbias",
+		"scaleType":9,
+		"stimType":"filler",
+		"list":"all"
+	},{
+		"story":"quadruplets",
+		//"storyline":"FIRST is the father of quadruplets.",
+		"storyline":"FIRST is the mother of quadruplets.",
+		"tag":"filler_lowbias",
+		"scaleType":9,
+		"stimType":"filler",
+		"list":"all"
+	}
+	]
+
+var scales = [
+	{weak: "or", strong: "and"},
+	{weak: "some", strong: "all"},
+	{weak: "looks like", strong: "is"},
+	{weak: "possible", strong: "certain"},
+	{weak: "three", strong: "more than three"}
+]
+
+scales[9] = "filler"
+ 
+// var stories = [
+// 	{
+// 		story: "or/and stealing",
+// 		storyline: "FIRST has stolen a car and a bicycle.",
+// 		tag: "evoke",
+// 		scaleType: 0,
+// 	},
+// 	{
+// 		story: "or/and violent crime",
+// 		storyline: "FIRST has been found guilty of theft and violent crime.",
+// 		tag: "evoke",
+// 		scaleType: 0,
+// 	},
+// 	{
+// 		story: "or/and threat",
+// 		storyline: "FIRST has threatened a judge and a police officer.",
+// 		tag: "evoke",
+// 		scaleType: 0,
+// 	},
+// 	{
+// 		story: "or/and not stealing",
+// 		storyline: "FIRST has had his car and his bicycle stolen.",
+// 		tag: "repress",
+// 		scaleType: 0,
+// 	},
+// 	{
+// 		story: "or/and not violent crime",
+// 		storyline: "FIRST has been a victim of theft and violence.",
+// 		tag: "repress",
+// 		scaleType: 0,
+// 	},
+// 	{
+// 		story: "or/and not police",
+// 		storyline: "FIRST has worked as a judge and a police officer.",
+// 		tag: "repress",
+// 		scaleType: 0,
+// 	},
+// 	{
+// 		story: "some/all criminal",
+// 		storyline: "All of FIRST's friends are criminals.",
+// 		tag: "evoke",
+// 		scaleType: 1,
+// 	},
+// 	{
+// 		story: "some/all violent",
+// 		storyline: "All of FIRST's brothers are violent.",
+// 		tag: "evoke",
+// 		scaleType: 1,
+// 	},
+// 	{
+// 		story: "some/all prison",
+// 		storyline: "All of FIRST's cousins have spent time in prison.",
+// 		tag: "evoke",
+// 		scaleType: 1,
+// 	},	
+// 	{
+// 		story: "some/all respect",
+// 		storyline: "All of FIRST's friends respect the police.",
+// 		tag: "repress",
+// 		scaleType: 1,
+// 	},
+// 	{
+// 		story: "some/all police",
+// 		storyline: "All of FIRST's brothers are police officers.",
+// 		tag: "repress",
+// 		scaleType: 1,
+// 	},
+// 	{
+// 		story: "some/all prison work",
+// 		storyline: "All of FIRST's cousins work at a prison.",
+// 		tag: "repress",
+// 		scaleType: 1,
+// 	},
+// 	{
+// 		story: "looks like/is criminal",
+// 		storyline: "FIRST is a criminal.",
+// 		tag: "evoke",
+// 		scaleType: 2,
+// 	},
+// 	{
+// 		story: "looks like/is thief",
+// 		storyline: "FIRST is a thief.",
+// 		tag: "evoke",
+// 		scaleType: 2,
+// 	},
+// 	{
+// 		story: "looks like/is dangerous",
+// 		storyline: "FIRST is a dangerous person.",
+// 		tag: "evoke",
+// 		scaleType: 2,
+// 	},
+// 	{
+// 		story: "looks like/is police",
+// 		storyline: "FIRST is a police officer.",
+// 		tag: "repress",
+// 		scaleType: 2,
+// 	},
+// 	{
+// 		story: "looks like/is judge",
+// 		storyline: "FIRST is a judge.",
+// 		tag: "repress",
+// 		scaleType: 2,
+// 	},
+// 	{
+// 		story: "looks like/is respect",
+// 		storyline: "FIRST is someone who respects the law.",
+// 		tag: "repress",
+// 		scaleType: 2,
+// 	},
+// 	{
+// 		story: "possible/certain jail",
+// 		storyline: "It is certain that FIRST is an inmate.",
+// 		tag: "evoke",
+// 		scaleType: 3,
+// 	},
+// 	{
+// 		story: "possible/certain stealing",
+// 		storyline: "It is certain that FIRST stole a TV from Walmart last week.",
+// 		tag: "evoke",
+// 		scaleType: 3,
+// 	},
+// 	{
+// 		story: "possible/certain violence",
+// 		storyline: "It is certain that FIRST has attacked someone at the park.",
+// 		tag: "evoke",
+// 		scaleType: 3,
+// 	},
+// 	{
+// 		story: "possible/certain not jail",
+// 		storyline: "It is certain that FIRST is a prison guard.",
+// 		tag: "repress",
+// 		scaleType: 3,
+// 	},
+// 	{
+// 		story: "possible/certain police",
+// 		storyline: "It is certain that FIRST wants to be a police officer.",
+// 		tag: "repress",
+// 		scaleType: 3,
+// 	},
+// 	{
+// 		story: "possible/certain not violence",
+// 		storyline: "It is certain that FIRST avoids violent people.",
+// 		tag: "repress",
+// 		scaleType: 3,
+// 	},
+// 	{
+// 		story: "may/will prison",
+// 		storyline: "FIRST will one day be sent to prison.",
+// 		tag: "evoke",
+// 		scaleType: 4,
+// 	},
+// 	{
+// 		story: "may/will violent",
+// 		storyline: "FIRST will one day commit a violent crime.",
+// 		tag: "evoke",
+// 		scaleType: 4,
+// 	},
+// 	{
+// 		story: "may/will no police",
+// 		storyline: "FIRST will one day attack a police officer.",
+// 		tag: "evoke",
+// 		scaleType: 4,
+// 	},
+// 	{
+// 		story: "may/will police",
+// 		storyline: "FIRST will one day join the police force.",
+// 		tag: "repress",
+// 		scaleType: 4,
+// 	},
+// 	{
+// 		story: "may/will crime",
+// 		storyline: "FIRST will one day get mugged.",
+// 		tag: "repress",
+// 		scaleType: 4,
+// 	},
+// 	{
+// 		story: "may/will judge",
+// 		storyline: "FIRST will become a judge.",
+// 		tag: "repress",
+// 		scaleType: 4,
+// 	},
+// 	{
+// 		story: "n/n+m crime",
+// 		storyline: "FIRST has committed more than three violent crimes.",
+// 		tag: "evoke",
+// 		scaleType: 5,
+// 	},
+// 	{
+// 		story: "n/n+m prison",
+// 		storyline: "FIRST has been sent to prison more than three times.",
+// 		tag: "evoke",
+// 		scaleType: 5,
+// 	},
+// 	{
+// 		story: "n/n+m murder",
+// 		storyline: "FIRST has threatened to kill more than three people.",
+// 		tag: "evoke",
+// 		scaleType: 5,
+// 	},
+// 	{
+// 		story: "n/n+m stolen",
+// 		storyline: "FIRST has had his wallet stolen more than three times.",
+// 		tag: "repress",
+// 		scaleType: 5,
+// 	},
+// 	{
+// 		story: "n/n+m victims",
+// 		storyline: "FIRST knows more than three victims of violent crime.",
+// 		tag: "repress",
+// 		scaleType: 5,
+// 	},
+// 	{
+// 		story: "n/n+m police",
+// 		storyline: "FIRST has more than three relatives who are police officers.",
+// 		tag: "repress",
+// 		scaleType: 5,
+// 	}
+// ]
